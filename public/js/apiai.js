bd9111ad8b28dee22da316e0afec3589609ad2a5
HyeJune Suh <hyejune.suh@gmail.com>
2017-11-24 18:47:12 +0900

    changed apiai.js (#undefined issue)

--- public/js/apiai.js
+++ public/js/apiai.js
@@ -137,7 +137,7 @@ function addTaskWithPerson(givenname, tasktitle) {
                   </li>`
 
                   var $newCard = $(newCard);
-                  $('#undef + .list-inner-scroll').last().prepend($newCard);
+                  $('#Undefined + .list-inner-scroll').last().prepend($newCard);
                   // $newCard.focus();
 
                   // console.log(result['task']['importance']);
